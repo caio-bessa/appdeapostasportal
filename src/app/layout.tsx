5919a8b6969914ab2f2969d335cbae7d138e493f
caio-bessa <157669325+caio-bessa@users.noreply.github.com>
2025-09-02 23:29:52 -0300

    Fix CSS import path in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import '@/app/globals.css'
+import '../../frontend/appdeapostas/src/app/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
